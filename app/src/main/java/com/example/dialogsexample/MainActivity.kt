671664ec7dab96b265b5e3e658151205a993d2ae
andersbor <anbo@zealand.dk>
2024-06-08 13:24:09 +0200

    Initial commit

---
+++ app/src/main/java/com/example/dialogsexample/MainActivity.kt
@@ -0,0 +1,278 @@
+package com.example.dialogsexample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import com.example.dialogsexample.ui.theme.DialogsExampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DialogsExampleTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(modifier = Modifier.padding(innerPadding)) {
+                        var openAlertDialog by remember { mutableStateOf(false) }
+                        var openMinimalDialog by remember { mutableStateOf(false) }
+                        var openImageDialog by remember { mutableStateOf(false) }
+                        var openAuthDialog by rememberSaveable { mutableStateOf(false) }
+
+                        Row {
+                            Button(onClick = { openAlertDialog = true }) {
+                                Text("Alert Dialog")
+                            }
+                            Button(onClick = { openMinimalDialog = true }) {
+                                Text("Minimal Dialog")
+                            }
+                        }
+                        Row {
+                            Button(onClick = { openImageDialog = true }) {
+                                Text("Dialog with Image")
+                            }
+                            Button(onClick = { openAuthDialog = true }) {
+                                Text("Auth Dialog")
+                            }
+                        }
+
+                        if (openAlertDialog) {
+                            AlertDialogExample(
+                                onDismissRequest = { openAlertDialog = false },
+                                onConfirmation = { openAlertDialog = false },
+                                dialogTitle = "Dialog Title",
+                                dialogText = "Dialog Text",
+                                icon = Icons.Filled.AddCircle
+                            )
+                        }
+                        if (openMinimalDialog) {
+                            MinimalDialog(
+                                onDismissRequest = { openMinimalDialog = false }
+                            )
+                        }
+                        if (openImageDialog) {
+                            DialogWithImage(
+                                onDismissRequest = { openImageDialog = false },
+                                onConfirmation = { openImageDialog = false },
+                                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                                imageDescription = "Example Image"
+                            )
+                        }
+                        var message by remember { mutableStateOf("") }
+                        fun checkEmailPassword(email: String, password: String) {
+                            if (email == "anbo@zealand.dk" && password == "secret12") {
+                                openAuthDialog = false
+                            } else {
+                                message = "Wrong email or password"
+                            }
+                        }
+                        if (openAuthDialog) {
+                            AuthDialog(
+                                onSignIn = { email, password ->
+                                    checkEmailPassword(
+                                        email,
+                                        password
+                                    )
+                                },
+                                onSignUp = { openAuthDialog = false },
+                                onCancel = { openAuthDialog = false },
+                                message = message
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+// https://developer.android.com/develop/ui/compose/components/dialog
+@Composable
+fun AlertDialogExample(
+    onDismissRequest: () -> Unit,
+    onConfirmation: () -> Unit,
+    dialogTitle: String,
+    dialogText: String,
+    icon: ImageVector,
+) {
+    AlertDialog(
+        icon = { Icon(icon, contentDescription = "Example Icon") },
+        title = { Text(text = dialogTitle) },
+        text = { Text(text = dialogText) },
+        onDismissRequest = { onDismissRequest() },
+        confirmButton = {
+            TextButton(
+                onClick = { onConfirmation() }
+            ) { Text("Confirm") }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = { onDismissRequest() }
+            ) { Text("Dismiss") }
+        }
+    )
+}
+
+@Composable
+fun MinimalDialog(onDismissRequest: () -> Unit) {
+    Dialog(onDismissRequest = { onDismissRequest() }) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .padding(16.dp),
+            shape = RoundedCornerShape(16.dp),
+        ) {
+            Text(
+                text = "This is a minimal dialog",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .wrapContentSize(Alignment.Center),
+                textAlign = TextAlign.Center,
+            )
+        }
+    }
+}
+
+@Composable
+fun DialogWithImage(
+    onDismissRequest: () -> Unit,
+    onConfirmation: () -> Unit,
+    painter: Painter,
+    imageDescription: String,
+) {
+    Dialog(onDismissRequest = { onDismissRequest() }) {
+        // Draw a rectangle shape with rounded corners inside the dialog
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(375.dp)
+                .padding(16.dp),
+            shape = RoundedCornerShape(16.dp),
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Image(
+                    painter = painter,
+                    contentDescription = imageDescription,
+                    contentScale = ContentScale.Fit,
+                    modifier = Modifier.height(160.dp)
+                )
+                Text(
+                    text = "This is a dialog with buttons and an image.",
+                    modifier = Modifier.padding(16.dp),
+                )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center,
+                ) {
+                    TextButton(
+                        onClick = { onDismissRequest() },
+                        modifier = Modifier.padding(8.dp),
+                    ) { Text("Dismiss") }
+                    TextButton(
+                        onClick = { onConfirmation() },
+                        modifier = Modifier.padding(8.dp),
+                    ) { Text("Confirm") }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun AuthDialog(
+    onSignIn: (email: String, password: String) -> Unit,
+    onSignUp: () -> Unit = {},
+    onCancel: () -> Unit = { },
+    message: String = "",
+) {
+    Card(modifier = Modifier.padding(16.dp), shape = RoundedCornerShape(16.dp))
+    {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        )
+        {
+            var email by remember { mutableStateOf("") }
+            var password by remember { mutableStateOf("") }
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Email") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
+            )
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Password") },
+                // https://alitalhacoban.medium.com/show-hide-password-jetpack-compose-d0c4abac568f
+                visualTransformation = PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+            )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+            ) {
+                Button(onClick = onCancel) {
+                    Text("Cancel")
+                }
+                Button(onClick = { onSignIn(email, password) }) {
+                    Text("Sign in")
+                }
+                Button(onClick = onSignUp) {
+                    Text("Sign up")
+                }
+            }
+            Text(text = message)
+        }
+    }
+}
+
+
+
